700483e967cae3b8c35f543de19c3d586eac3fbe
minsung <menysung94@naver.com>
2024-07-02 12:23:57 +0900

    로그인페이지 구현

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,3 +1,89 @@
+import { FirebaseError } from "firebase/app";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import "./Login.css";
+
 export default function Login() {
-  return <div>Login</div>;
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // 버튼 한 번 더 클릭하면 에러메시지 초기화
+    setError("");
+    if (isLoading || name === "" || email === "" || password === "") return;
+    try {
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        //console.log(e.code, e.message);
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <h1 className="title">Login 𝕏</h1>
+      <form className="form" onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+          className="input"
+        />
+        <input
+          onChange={onChange}
+          value={password}
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+          className="input"
+        />
+        <input
+          type="submit"
+          value={isLoading ? "Loading..." : "Log in"}
+          className="input submit"
+        />
+      </form>
+      {error !== "" && <span className="error">{error}</span>}
+      <div className="switcher">
+        <span>계정이 없으신가요? </span>
+        <Link to="/createaccount">가입하기 &rarr;</Link>
+      </div>
+    </div>
+  );
 }
